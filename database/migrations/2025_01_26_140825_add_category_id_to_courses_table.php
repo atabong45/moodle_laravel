38e49c38b85e09b6f1b50dfb6f1c84f7c5055955
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2025-01-26 15:24:00 +0100

    create category controllerr

---
+++ database/migrations/2025_01_26_140825_add_category_id_to_courses_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdTocoursesTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable(); // Permettre une valeur null au cas où
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+    }
+}
