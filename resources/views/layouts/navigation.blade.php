bd862cf1bf9065fbdbf9b739a4e4c19b643486d2
NOMO-Gabriel <gabriel.nomo@facsciences-uy1.cm>
2025-01-27 06:27:53 +0100

    roles configs are okay

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,23 +12,14 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('courses.index')" :active="request()->routeIs('courses.index')">
                         {{ __('Cours') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('modules.index')" :active="request()->routeIs('modules.index')">
-                        {{ __('Modules') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('sections.index')" :active="request()->routeIs('sections.index')">
-                        {{ __('Sections') }}
                     </x-nav-link>
                     <x-nav-link :href="route('assignments.index')" :active="request()->routeIs('assignments.index')">
                         {{ __('Évaluations') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                        {{ __('Profil') }}
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Tableau de bord') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -82,23 +73,14 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('courses.index')" :active="request()->routeIs('courses.index')">
                 {{ __('Cours') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('modules.index')" :active="request()->routeIs('modules.index')">
-                {{ __('Modules') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('sections.index')" :active="request()->routeIs('sections.index')">
-                {{ __('Sections') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('assignments.index')" :active="request()->routeIs('assignments.index')">
                 {{ __('Évaluations') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                {{ __('Profil') }}
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
 
